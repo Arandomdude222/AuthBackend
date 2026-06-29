c72a0bd86d6fabae3b7ae4ab0adc0b18778acf51
ard22 <ard22>
2026-06-29 20:25:19 +0100

    Pass username to client on login

--- api/login.js
+++ api/login.js
@@ -36,7 +36,7 @@ module.exports = async (req, res) => {
     const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
     // Return structure expected by frontend
-    res.status(200).json({ session: { access_token: token } });
+    res.status(200).json({ session: { access_token: token, username: username } });
   } catch (err) {
     res.status(500).json({ error: 'Internal server error' });
   }
